c53c18f9253a8e4922de4ee0b73bceca1473d719
khantMinHtut <khantminhtut009@gmail.com>
2025-01-06 17:09:20 +0630

    Delete Some unnecessary components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,3 @@
-import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
-import SushiIcon from "../Icon/SushiIcon";
-import CallNowBtn from "../buttons/CallNowBtn";
 import Dropdown from "./nav/DropDown";
 
 const Header = () => {
