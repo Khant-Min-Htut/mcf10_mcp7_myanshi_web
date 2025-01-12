3b9b52a1dc4769ac2fde2ab1e945fd9d01a07db9
khantMinHtut <khantminhtut009@gmail.com>
2025-01-12 12:37:33 +0630

    shushi icon

--- src/Icon/SushiIcon.jsx
+++ src/Icon/SushiIcon.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SushiIcon = () => {
   return (
     <img
-      src="/src/assets/SushiIcon.png"
+      src="/src/assets/ShushiIcon.png"
       alt="Static Icon"
       width={43} // Fixed width
       height={50} // Fixed height
