3e1fc1a57e19910d636b812e18c7ee3fc9a6d1cf
khantMinHtut <khantminhtut009@gmail.com>
2025-01-12 12:28:07 +0630

    change header button color

--- src/buttons/CallNowBtn.jsx
+++ src/buttons/CallNowBtn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CallNowBtn = ({ text, className }) => {
   return (
     <button
-      className={`bg-white h-[54px] w-[149px] px-8 py-4 gap-[10px] transition duration-300 hover:bg-[rgba(184,26,0,1)] ${className}`}
+      className={`bg-primary-color h-[54px] w-[149px] px-8 py-4 gap-[10px] transition duration-300 hover:bg-[rgba(184,26,0,1)] ${className}`}
     >
       <span className="text-[14px] leading-[21.6px] font-[700] text-white font-satoshi w-[85px] h-[22px]">
         {text}
