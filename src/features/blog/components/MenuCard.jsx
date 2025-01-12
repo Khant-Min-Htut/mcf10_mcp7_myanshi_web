d5410dec92b480ee57ca9fbc371a63911ef83163
JuSan7 <hanthijusan.wytu2012@gmail.com>
2025-01-12 12:53:43 +0630

    added feat/blog-detail

--- src/features/blog/components/MenuCard.jsx
+++ src/features/blog/components/MenuCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import { TfiLayoutLineSolid } from "react-icons/tfi";
 import { Link } from "react-router-dom";
 import Pagination from "./Pagination";
 
@@ -8,39 +9,54 @@ const MenuCard = ({ data }) => {
     <>
       {data.map((item) => (
         <div
-          className="flex flex-col md:flex-row-reverse gap-7 mb-12 last:mb-0"
+          className="flex flex-col md:flex-row-reverse gap-7 mb-12 last:mb-0 group"
           key={item.title}
         >
           <div className="imgLeft flex-1">
             <img
               src={item.image}
               alt="menuImg"
-              className="h-64 md:h-[300px] lg:h-[350px] w-full object-cover"
+              className="h-[300px] md:h-[350px] lg:h-[400px] w-full object-cover transition-all duration-300 group-hover:scale-110"
             />
           </div>
           <div className="menuDetailRight flex-1 md:flex md:justify-center md:items-center">
             <div className="flex flex-col gap-2">
               <div className="flex gap-4 font-satoshi text-bodySmall lg:text-bodyLarge text-neutral-600">
                 <span>{item.chefName}</span>
-                <span>___</span>
+                <span className="flex items-center">
+                  <TfiLayoutLineSolid />
+                  <TfiLayoutLineSolid />
+                </span>
                 <span>{item.Date}</span>
               </div>
-              <h1 className="font-notoSerifJp text-heading2 lg:text-heading1">
+              <h1 className="font-notoSerifJp text-heading2 lg:text-heading1 group-hover:text-primary-color">
                 {item.title}
               </h1>
-              <p className="text-bodySmall text-neutral-600 mb-5 lg:text-bodyLarge">
+              <p className="text-bodySmall text-neutral-600 mb-5 lg:text-bodyLarge ">
                 {item.desc}
               </p>
-              <Link to={"#"}>
-                <button className="flex justify-between items-center font-satoshi lg:text-bodyLarge">
+              <Link to={`/blog/${item.id}`}>
+                <button className="flex justify-between items-center font-satoshi lg:text-bodyLarge group-hover:text-primary-color">
                   READ MORE
-                  <MdKeyboardArrowRight size={24} className="block lg:hidden" />
-                  <MdKeyboardArrowRight size={28} className="hidden lg:block" />
+                  <MdKeyboardArrowRight
+                    size={24}
+                    className="block lg:hidden group-hover:text-primary-color transform group-hover:translate-x-1 duration-300"
+                  />
+                  <MdKeyboardArrowRight
+                    size={28}
+                    className="hidden lg:block group-hover:text-primary-color transform group-hover:translate-x-1 duration-300"
+                  />
                 </button>
               </Link>
             </div>
           </div>
         </div>
+        // <ArticlesCard
+        //   date={item.Date}
+        //   title={item.title}
+        //   description={item.desc}
+        //   img={item.image}
+        // />
       ))}
       <div className="w-full">
         <Pagination />
--- src/features/detail-blog/pages/DetailBlog.jsx
+++ src/features/detail-blog/pages/DetailBlog.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import HeroSection from "../../../components/sectionComponents/HeroSection";
+import BlogSection from "../components/BlogSection";
 
 const DetailBlog = () => {
-  return <div>DetailBlog</div>;
+  const links = [
+    {
+      path: "/blog",
+      title: "Blog",
+    },
+  ];
+  return (
+    <div>
+      <HeroSection
+        isBreadCrumb={true}
+        currentPage={"Gluten-Free Sushi"}
+        title="How to prepare a delicious gluten-free sushi"
+        links={links}
+      />
+      <BlogSection />
+    </div>
+  );
 };
 
 export default DetailBlog;
